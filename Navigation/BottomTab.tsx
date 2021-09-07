c7a552a8a3eb5fc6225437a2a919d31a9c990975
FrancyelZelling <zellingf@gmail.com>
2021-09-07 16:12:05 -0300

    Styled the bottom tab navbar

--- Navigation/BottomTab.tsx
+++ Navigation/BottomTab.tsx
@@ -1,6 +1,7 @@
 import * as React from "react"
-import { Text, View } from "react-native"
+import {  StyleSheet} from "react-native"
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import { Feather } from '@expo/vector-icons'
 
 import Home from "../Screens/Home"
 import Menu from "../Screens/Menu"
@@ -9,9 +10,32 @@ export default function BottomTab() {
     const Tab = createBottomTabNavigator()
 
     return (
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={Home}/>
-        <Tab.Screen name="Menu" component={Menu}/>
+      <Tab.Navigator initialRouteName="Home" tabBarOptions={{
+        tabStyle: styles.container,
+        showLabel: false
+      }}>
+        <Tab.Screen name="Menu" component={Menu} options={{
+          tabBarIcon:(tabinfo) => (
+            <Feather name="menu" size={26} color={tabinfo.focused ? "white" : "#353536"} />
+          )
+        }} />
+        <Tab.Screen name="Home" component={Home} options={{
+          tabBarIcon:(tabinfo) => (
+            <Feather name="home" size={26} color={tabinfo.focused ? "white" : "#353536"} />
+          )
+        }} />
+        <Tab.Screen name="Profile" component={Menu} options={{
+          tabBarIcon:(tabinfo) => (
+            <Feather name="user" size={26} color={tabinfo.focused ? "white" : "#353536"} />
+          )
+        }} />
       </Tab.Navigator>
     )
   }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#F0B322',
+    color: '#353536'
+  }
+})
--- Screens/Menu.tsx
+++ Screens/Menu.tsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 const Menu = () => {
   return (
     <View>
       <Text>Menu Component</Text>
     </View>
-  )
-}
+  );
+};
 
-export default Menu
+export default Menu;
 
-const styles = StyleSheet.create({})
-
+const styles = StyleSheet.create({});
