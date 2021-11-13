35f2369c7970cc92e10f38a96eadd702c02f83cf
FrancyelZelling <zellingf@gmail.com>
2021-11-13 10:01:23 -0300

    Finished Deliver component logic and styling

--- Components/Deliver/Deliver.tsx
+++ Components/Deliver/Deliver.tsx
@@ -1,5 +1,5 @@
 import React, { useState} from 'react'
-import { View, Text, TextInput, TouchableOpacity } from 'react-native'
+import { View, Text, TextInput, TouchableOpacity, Pressable } from 'react-native'
 import { FontAwesome } from '@expo/vector-icons';
 import { styles } from './Styles';
 
@@ -8,32 +8,50 @@ const Deliver = () => {
   const [district, onChangeDistrict] = useState("")
   const [addressNumber, onChangeAdressNumber] = useState("")
   const [notes, onChangeNotes] = useState("")
+  const [money, setMoney] = useState(false)
+  const [creditCard, setCreditCard] = useState(false)
+
+  const changePayment = (moneyPayment: boolean) => {
+   if (moneyPayment){
+    setCreditCard(false)
+    setMoney(!money)
+   } else {
+     setMoney(false)
+     setCreditCard(!creditCard)
+   }
+  }
 
   return (
     <View style={styles.container}>
       <View style={styles.infoContainer}>
         <View style={styles.addressContainer}>
           <Text style={styles.label}>Address</Text>
-          <TextInput style={styles.addressInput} onChangeText={onChangeAdress} value={address} keyboardType="default" />
+          <TextInput style={[styles.addressInput, styles.input]} onChangeText={onChangeAdress} value={address} keyboardType="default" />
         </View>
-        <View style={styles.districkContainer}>
-          <Text style={styles.label}>District</Text>
-          <TextInput style={styles.districtInput} onChangeText={onChangeDistrict} value={district} keyboardType="default" />
-        </View>
-        <View style={styles.addressNumberContainer}>
-          <Text style={styles.label}>Address Number</Text>
-          <TextInput style={styles.addressNumberContainer} onChangeText={onChangeAdressNumber} value={addressNumber} keyboardType="default" />
+        <View style={styles.smallContainer}>
+          <View style={styles.districtContainer}>
+            <Text style={styles.label}>District</Text>
+            <TextInput style={[styles.districtInput, styles.input]} onChangeText={onChangeDistrict} value={district} keyboardType="default" />
+          </View>
+          <View style={styles.addressNumberContainer}>
+            <Text style={styles.label}>Address Number</Text>
+            <TextInput style={[styles.addressNumberInput, styles.input]} onChangeText={onChangeAdressNumber} value={addressNumber} keyboardType="default" />
+          </View>
         </View>
         <View style={styles.notesContainer}>
           <Text style={styles.label}>Notes</Text>
-          <TextInput style={styles.notesContainer} onChangeText={onChangeNotes} value={notes} keyboardType="default" multiline numberOfLines={2} />
+          <TextInput style={[styles.notesInput, styles.input]}  onChangeText={onChangeNotes} value={notes} keyboardType="default" multiline numberOfLines={2} />
         </View>
       </View>
       <View style={styles.paymentContainer}>
         <Text style={styles.paymentTitle}>Payment Method</Text>
         <View style={styles.iconsContainer}>
-          <FontAwesome name="money" size={48} color="white" />
-          <FontAwesome name="credit-card" size={48} color="white" />
+          <Pressable onPress={() => changePayment(true)}>
+            <FontAwesome style={money ? [styles.icon, styles.iconFocused] : styles.icon} name="money" size={60} color="white" />
+          </Pressable>
+          <Pressable onPress={() => changePayment(false)}>
+            <FontAwesome style={creditCard ? [styles.icon, styles.iconFocused] : styles.icon}name="credit-card" size={54} color="white"/>
+          </Pressable>
         </View>
         <TouchableOpacity style={styles.button}>
           <Text style={styles.buttonText}>Confirm Order</Text>
--- Components/Deliver/Styles.ts
+++ Components/Deliver/Styles.ts
@@ -1,20 +1,96 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-  container: {},
-  infoContainer: {},
-  addressContainer: {},
-  label: {},
-  addressInput: {},
-  districtContainer: {},
-  districtInput: {},
-  addressNumberContainer: {},
-  addressNumberInput: {},
-  notesContainer: {},
-  notesInput: {},
-  paymentContainer: {},
-  paymentTitle: {},
-  iconsContainer: {},
-  button: {},
-  buttonText: {},
+  container: {
+    marginHorizontal: 30,
+    marginTop: 15,
+  },
+  infoContainer: {
+    display: "flex",
+    borderColor: "#f0b322",
+    paddingBottom: 6,
+    marginBottom: 12,
+    borderBottomWidth: 2
+  },
+  addressContainer: {
+  },
+  smallContainer: {
+    flexDirection: "row"
+  },
+  label: {
+    color: "#f0b322",
+    fontSize: 18,
+    marginLeft: 10
+  },
+  input: {
+    backgroundColor: '#3c3c3d',
+    borderColor: "#f0b322",
+    borderWidth: 1,
+    borderRadius: 6,
+    marginTop: 4,
+    paddingHorizontal: 10,
+    color: "white",
+    fontSize: 16
+  },
+  addressInput: {
+    height: 40,
+  },
+  districtContainer: {
+    flex: 1,
+    marginRight: 16,
+    marginTop: 20
+  },
+  districtInput: {
+    height: 40,
+  },
+  addressNumberContainer: {
+    flex: 1,
+    marginTop: 20
+  },
+  addressNumberInput: {
+    height: 40,
+  },
+  notesContainer: {
+    marginTop: 20
+  },
+  notesInput: {
+  },
+  paymentContainer: {
+    display: "flex",
+    alignItems: "center",
+    marginTop: 20
+  },
+  paymentTitle: {
+    fontSize: 24,
+    color: "#f0b322",
+    fontWeight: "300",
+  },
+  iconsContainer: {
+    display: "flex",
+    flexDirection: "row",
+    marginVertical: 12,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  icon: {
+    color: "white",
+    marginHorizontal: 10
+  },
+  iconFocused: {
+    color: "#f0b322"
+  },
+  button: {
+    width: 300,
+    height: 35,
+    backgroundColor: "#f0b322",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 24,
+    marginTop: 10
+  },
+  buttonText: {
+    color: "#353536",
+    fontSize: 18,
+    fontWeight: "600"
+  },
 })
